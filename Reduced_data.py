9ac72da9a26299e913f54dda2bd879688cd21c83
Sara Rivera Méndez <rsaracontacto@gmail.com>
2026-03-02 22:55:15 -0600

    :(

--- Reduced_data.py
+++ Reduced_data.py
@@ -9,7 +9,7 @@ df = df[["Sem_ISO", "ProductoID", "estacion", "Estado"]]
 # Agregar
 df_grouped = df.groupby(
     ["Sem_ISO", "ProductoID", "estacion"]
-)["Cantidad"].sum().reset_index()
+)["Estado"].sum().reset_index()
 
 # Guardar versión ligera
 df_grouped.to_csv("ventas_light_n.csv", index=False)
