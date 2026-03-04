b3898c26e76a07360b61e8722f1c75122f913a6d
SaraRiveraMendez <rsaracontacto@gmail.com>
2026-03-04 16:47:06 -0600

    Better looks

--- Reduced_data.py
+++ Reduced_data.py
@@ -4,14 +4,14 @@ import pandas as pd
 df = pd.read_csv("C:/Users/rsara/Downloads/consolidado_negado_nacional.csv")
 
 # Quedarnos solo con columnas necesarias
-df = df[["Sem_ISO", "ProductoID", "estacion", "Estado"]]
+df = df[["Sem_ISO", "ProductoID", "estacion", "Estado", "Negados"]]
 
 # Agregar
 df_grouped = df.groupby(
-    ["Sem_ISO", "ProductoID", "estacion"]
+    ["Sem_ISO", "ProductoID", "estacion", "Negados"]
 )["Estado"].sum().reset_index()
 
 # Guardar versión ligera
-df_grouped.to_csv("ventas_light_n.csv", index=False)
+df_grouped.to_csv("ventas_light_n2.csv", index=False)
 
 print("Archivo reducido creado.")
